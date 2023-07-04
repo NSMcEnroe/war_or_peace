219467162b9d5913208cbea9fa35000d9e4e7954
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-04 00:24:48 -0400

    Define rank_of_card_at

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,5 +11,25 @@ RSpec.describe Card do
     expect(card2.suit).to eq(:spade)
     expect(card2.value).to eq('3')
     expect(card2.rank).to eq(3)
+
+    cards = [card1, card2, card3]
     end
 end
+
+RSpec.describe Deck do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+    
+    deck = Deck.new(cards)
+
+    # p deck
+    # p deck.cards
+    # p deck.rank_of_card_at(0)
+    # p deck.rank_of_card_at(2)
+    
+
+end
+
